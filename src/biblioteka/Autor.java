c1bf68ce28beda511694f46b8e690d03c7c836ef
Saponja <jsaponjic512@gmail.com>
2021-03-15 15:44:00 +0100

    Dokumetovan autor i interfejs IBiblioteka

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -1,25 +1,72 @@
 package biblioteka;
 
-public class Autor {
 
+/**
+ * 
+ *  Klasa koja predstavlja autora knjige
+ *  
+ *  Autor ima ime i prezime kao String vrednosti
+ *  
+ *  
+ *  
+ *  @author Jovan Saponjic
+ *  @version 0.1
+ *  
+ *
+ *  */
+
+
+public class Autor {
+	
+	
+	/** 
+	 * Ime autora kao string
+	 * */
 	private String ime;
+	/** 
+	 * Prezime autora kao string
+	 * */
 	private String prezime;
 	
-	
+	/** 
+	 * Konstruktor koji inicijalizuje objekat
+	 * */
 	public Autor() {
 		super();
 	}
 	
-	
+	/** 
+	 * 
+	 * Konstruktor koji incijalizuje objekat i postavlja vrednosti za ime i prezime autora.
+	 * 
+	 * @param ime Ime autora kao String
+	 * @param prezime Prezime autora kao String
+	 * 
+	 * */
 	public Autor(String ime, String prezime) {
 		super();
 		setIme(ime);
 		setPrezime(prezime);
 	}
 	
+	/**
+	 * Vraca ime autora kao string
+	 * 
+	 * @return Ime autora kao string
+	 *  */
+	
 	public String getIme() {
 		return ime;
 	}
+	
+	/**
+	 * Postavlja ime autora na novu vrednost
+	 * 
+	 * @param ime Ime auotra kao string
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneto ime null
+	 * @throws java.lang.RuntimeException Ako je uneto ime koja ima manje od 2 znaka
+	 *  */
 	public void setIme(String ime) {
 		
 		if(ime == null) {
@@ -31,10 +78,24 @@ public class Autor {
 		
 		this.ime = ime;
 	}
+	
+	/**
+	 * Vraca prezime autora
+	 * 
+	 * @return Prezime autora kao string
+	 *  */
 	public String getPrezime() {
 		
 		return prezime;
 	}
+	/**
+	 * Postavlja prezime autora na novu vrednost
+	 * 
+	 * @param ime Prezime auotra kao string
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneto prezime null
+	 * @throws java.lang.RuntimeException Ako je uneto prezime koja ima manje od 2 znaka
+	 *  */
 	public void setPrezime(String prezime) {
 		
 		if(prezime == null) {
@@ -46,6 +107,12 @@ public class Autor {
 		
 		this.prezime = prezime;
 	}
+	
+	/** 
+	 * Vraca string sa svim podacima o autoru
+	 * 
+	 * @return vraca String sa podacima o autoru
+	 * */
 	@Override
 	public String toString() {
 		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
@@ -58,6 +125,19 @@ public class Autor {
 		result = prime * result + ((prezime == null) ? 0 : prezime.hashCode());
 		return result;
 	}
+	
+	
+	/** 
+	 * Poredi 2 autora i vraca true ako su isti, a false ako nisu.
+	 * 
+	 * Autori se porede po imenu i prezimenu i oba moraju da budu ista.
+	 * 
+	 * @return 
+	 * <ul>
+	 * 		<li>true ako su oba objekta klase Autor i imaju ista imena i prezimena</li>
+	 * 		<li>false u svim ostalim slucajevima</li>
+	 * </ul>
+	 * */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
--- src/biblioteka/interfejs/IBiblioteka.java
+++ src/biblioteka/interfejs/IBiblioteka.java
@@ -2,11 +2,29 @@ package biblioteka.interfejs;
 
 import java.util.LinkedList;
 
+/**
+ * 
+ *  Interfejs koji predstavlja biblioteku
+ *  
+ *  @author Jovan Saponjic
+ *  
+ *  */
+
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
 public interface IBiblioteka {
-
+	
+	
+	/**
+	 * Dodaje novu knjigu u biblioteku
+	 * 
+	 * @param knjia Knjiga koja se dodaje u listu
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneta knjiga null
+	 * @throws java.lang.RuntimeException ako untea knjiga vec postoji u biblioteci
+	 *  */
+	
 	public void dodajKnjigu(Knjiga knjiga);
 	public void obrisiKnjigu(Knjiga knjiga);
 	public LinkedList<Knjiga> vratiSveKnjige();
